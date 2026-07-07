913095efb3022c855f450820b9983e7caaa8df2b
SimonF <simonfrancoguisado@gmail.com>
2026-07-07 00:57:32 -0500

    creacion de la funcion compararPokemons

--- pokedex.js
+++ pokedex.js
@@ -54,4 +54,52 @@ async function mostrarFicha() {
 
 }
 
-mostrarFicha();
+//mostrarFicha();
+
+function obtenerStats(datos, nombreStat) {
+    for (const stat of datos.stats) {
+        if (stat.stat.name === nombreStat) {
+            return stat.base_stat;
+        }
+    }
+    return null;
+}
+
+async function compararPokemons(nombre1, nombre2, stat) {
+
+    const datos1 = await buscarPokemon(nombre1);
+    const datos2 = await buscarPokemon(nombre2);
+    
+    const nombreStat = prompt("¿Qué stat quieres comparar? ").toLowerCase();
+
+    if ( datos1 === null || datos2 === null) {
+        console.log("Alguno de los pokemon no existen")
+        return;
+    }
+
+    const valor1 = obtenerStats(datos1, nombreStat);
+    const valor2 = obtenerStats(datos2, nombreStat);
+
+    if (valor1 === null || valor2 === null) {
+        console.log("stat inexistente");
+        console.log("Stats validas: hp, attack, defense, special-atack, special-defense, speed");
+        return;
+    }
+
+    console.log(datos1.name.toUpperCase() + " tiene " + nombreStat + ": " +  valor1);
+    console.log(datos2.name.toUpperCase() + " tiene " + nombreStat + ": " +  valor2);
+
+    if (valor1 > valor2) {
+
+        console.log("Gana " + datos1.name.toUpperCase());
+
+    } else if (valor2 > valor1) {
+
+        console.log("Gana " + datos2.name.toUpperCase());
+
+    } else {
+        console.log("empate en: " + nombreStat);
+    }
+}
+
+compararPokemons();
